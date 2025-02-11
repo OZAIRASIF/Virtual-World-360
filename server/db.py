8b60b663d0e9ef9eb1fd3d4a8ddb9aa13563ce91
Swikrit <swikki2003@gmail.com>
2025-02-12 02:09:54 +0530

    added configs for vercel

--- server/db.py
+++ server/db.py
@@ -1,11 +1,22 @@
-from pymongo import MongoClient
-from bson import ObjectId
+
 import os
+import pymongo
 
-# MongoDB client and database setup
-client = MongoClient("mongodb://localhost:27017")  # Adjust the connection string as necessary
-db = client.virtual_tour_db
+ENVIRONMENT = os.environ["ENVIRONMENT"]
 
-# Collections
-tours_collection = db.tours
-scenes_collection = db.scenes
+if ENVIRONMENT == "dev":
+    connection_string = "mongodb://localhost:27017"
+    db_client = pymongo.MongoClient(connection_string)
+    DB_NAME="virtual_tour_db"
+    db_client = db_client.get_database(DB_NAME)
+else:   
+    MONGO_CLUSTER = os.environ["MONGO_URI"]
+    MONGO_USERNAME = os.environ["MONGO_USERNAME"]
+    MONGO_PASSWORD = os.environ["MONGO_PASSWORD"]
+    DB_NAME = os.environ["DB_NAME"]
+    connection_string = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/?retryWrites=true&ssl=true&ssl_cert_reqs=CERT_NONE&w=majority"
+    db_client = pymongo.MongoClient(connection_string)
+    db_client = db_client.get_database(DB_NAME)
+
+tours_collection= db_client.get_collection("tours")
+scenes_collection = db_client.get_collection("scenes")
